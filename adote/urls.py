15437220896f617d018d845b160b5a0c7e33dadc
Bruno <brunoconcli@gmail.com>
2023-01-15 22:59:52 -0300

    ⚙️ | setting new url `adopt`

--- adote/urls.py
+++ adote/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('disclose/', include('disclose.urls')),
+    path('adopt/', include('adopt.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
